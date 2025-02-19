4b197c3786645b0bdbd607847546984af641e406
Khor Hsien Kit <hsienkitkhor@gmail.com>
2025-02-19 18:41:51 +0800

    Complete problem 1

---
+++ src/problem1/answer.js
@@ -0,0 +1,22 @@
+// Linear addition
+var sum_to_n_a = function(n) {
+    result = 0;
+    for (let i = 0; i <= n; i++) {
+        result += i;
+    }
+    return result;
+};
+
+// Recursive Linear addition
+var sum_to_n_b = function(n) {
+    if (n == 1) {
+        return n;
+    }
+    return n + sum_to_n_b(n - 1);
+};
+
+// Using sum of arithmetic progression
+var sum_to_n_c = function(n) {
+    result = (n / 2) * (2 + (n-1));
+    return result;
+};
