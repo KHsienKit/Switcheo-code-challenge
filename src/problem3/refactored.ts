346f1be6661a86051bd51a2f05a74b2e65bf98a0
Khor Hsien Kit <hsienkitkhor@gmail.com>
2025-02-25 14:11:47 +0800

    Add old code and refactored code

--- src/problem3/refactored.ts
+++ src/problem3/refactored.ts
@@ -1,95 +1,104 @@
-interface WalletBalance {
-    currency: string;
-    amount: number;
-}
-
-interface FormattedWalletBalance {
-    currency: string;
-    amount: number;
-    formatted: string;
-}
+import {WalletBalance, FormattedWalletBalance, Props} from "./types.ts";
+import axios from "axios";
   
 class Datasource {
-    // TODO: Implement datasource class
+    link: string;
+
+    constructor(link: string) {
+        this.link = link;
+    }
+
+    get getPrices(): Promise<AxiosResponse<any, any>> {
+        return axios.get(this.link);
+    }
 }
-  
-interface Props extends BoxProps {
-  
+
+const getPriority = (blockchain: string): number => {
+    switch (blockchain) {
+        case 'Osmosis':
+            return 100
+        case 'Ethereum':
+            return 50
+        case 'Arbitrum':
+            return 30
+        case 'Zilliqa':
+            return 20
+        case 'Neo':
+            return 20
+        default:
+            return -99
+    }
 }
-  const WalletPage: React.FC<Props> = (props: Props) => {
+
+const filterBalance = (balance: WalletBalance): boolean => {
+    const balancePriority = getPriority(balance.blockchain);
+    if (balancePriority > -99 && balance.amount <= 0) {
+        return true;
+    }
+    return false
+}
+
+const balanceCompareFunction = (lhs: WalletBalance, rhs: WalletBalance): number => {
+    const leftPriority = getPriority(lhs.blockchain);
+    const rightPriority = getPriority(rhs.blockchain);
+    if (leftPriority > rightPriority) {
+      return -1;
+    } else if (rightPriority > leftPriority) {
+      return 1;
+    }
+    return 0;
+}
+
+const WalletPage: React.FC<Props> = (props: Props) => {
     const { children, ...rest } = props;
     const balances = useWalletBalances();
-      const [prices, setPrices] = useState({});
+    const [prices, setPrices] = useState<Record<string, number>>({});
   
     useEffect(() => {
-      const datasource = new Datasource("https://interview.switcheo.com/prices.json");
-      datasource.getPrices().then(prices => {
-        setPrices(prices);
-      }).catch(error => {
-        console.err(error);
-      });
+        const datasource = new Datasource("https://interview.switcheo.com/prices.json");
+        datasource.getPrices.then((prices) => {
+            setPrices((prev) => {
+                for (let i = 0; i < data.length; i += 1) {
+                    prev[prices[i]["currency"]] = prices[i]["price"]
+                }
+                return prev;
+              })
+        }).catch(error => {
+            console.err(error);
+        });
     }, []);
   
-      const getPriority = (blockchain: any): number => {
-        switch (blockchain) {
-          case 'Osmosis':
-            return 100
-          case 'Ethereum':
-            return 50
-          case 'Arbitrum':
-            return 30
-          case 'Zilliqa':
-            return 20
-          case 'Neo':
-            return 20
-          default:
-            return -99
-        }
-      }
-  
     const sortedBalances = useMemo(() => {
-      return balances.filter((balance: WalletBalance) => {
-            const balancePriority = getPriority(balance.blockchain);
-            if (lhsPriority > -99) {
-               if (balance.amount <= 0) {
-                 return true;
-               }
-            }
-            return false
-          }).sort((lhs: WalletBalance, rhs: WalletBalance) => {
-              const leftPriority = getPriority(lhs.blockchain);
-            const rightPriority = getPriority(rhs.blockchain);
-            if (leftPriority > rightPriority) {
-              return -1;
-            } else if (rightPriority > leftPriority) {
-              return 1;
-            }
-      });
+        return balances.filter((balance: WalletBalance) => {
+            return filterBalance(balance);
+        }).sort((lhs: WalletBalance, rhs: WalletBalance) => {
+            return balanceCompareFunction(lhs, rhs);
+        });
     }, [balances, prices]);
   
     const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
-      return {
-        ...balance,
-        formatted: balance.amount.toFixed()
-      }
+        return {
+            ...balance,
+            formatted: balance.amount.toFixed()
+        }
     })
   
-    const rows = sortedBalances.map((balance: FormattedWalletBalance, index: number) => {
-      const usdValue = prices[balance.currency] * balance.amount;
-      return (
+    const rows = formattedBalances.map((balance: FormattedWalletBalance, index: number) => {
+        const usdValue = prices[balance.currency] * balance.amount;
+        return (
         <WalletRow 
-          className={classes.row}
-          key={index}
-          amount={balance.amount}
-          usdValue={usdValue}
-          formattedAmount={balance.formatted}
+            className={classes.row}
+            key={index}
+            amount={balance.amount}
+            usdValue={usdValue}
+            formattedAmount={balance.formatted}
         />
-      )
+        )
     })
   
     return (
-      <div {...rest}>
-        {rows}
-      </div>
+        <div {...rest}>
+            {rows}
+        </div>
     )
   }
