307ca66eac329c43ca1409e747742c1eefabf2a4
KHsienKit <hsienkitkhor@gmail.com>
2025-03-07 13:47:46 +0800

    Change transition delay to 100ms

--- src/problem2/my-app/src/Components/CurrencyMenu.tsx
+++ src/problem2/my-app/src/Components/CurrencyMenu.tsx
@@ -11,9 +11,9 @@ const CurrencyMenu = ({currency, changeCurrency, prices}: CurrencyMenuProps) => 
     return (
         <div className="relative flex flex-col text-xl">
             <button 
-                className={`${isDropdownFocused ? "rounded-t-2xl" : "rounded-3xl"} 
+                className={`${isDropdownFocused ? "rounded-t-2xl delay-0" : "rounded-3xl delay-100"} 
                             bg-gray-600 w-40 h-10 flex flex-row items-center justify-between hover:cursor-pointer hover:bg-gray-800
-                            transition-all delay-100`} 
+                            transition-all`} 
                 onClick={() => changeFocus(isDropdownFocused)} 
                 onBlur={() => setIsDropdownFocused(false)}>
                 <img 
@@ -22,7 +22,7 @@ const CurrencyMenu = ({currency, changeCurrency, prices}: CurrencyMenuProps) => 
                 <div className="flex-grow pr-6">{currency}</div>
                 <menu className={`${isDropdownFocused ? "flex max-h-48 " : " max-h-0 invisible "} 
                                     absolute flex-col w-40 mx-0 px-0 top-10 overflow-y-scroll bg-gray-600
-                                    transition-all duration-75 ease-linear`}>
+                                    transition-all duration-100 ease-linear`}>
                     {
                         Object.keys(prices).map((key) => {
                             return(
